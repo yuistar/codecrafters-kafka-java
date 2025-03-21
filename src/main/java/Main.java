e0d20f2d4eae5706d3a9b04776fc991f85baf2b9
yhuang-clari <yhuang@clari.com>
2025-03-21 07:53:23 -0700

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -38,55 +38,55 @@ public class Main {
             Body
                 error_code => INT16
         */
-        try {
+        if (input.available() == 0) {
+            return;
+        } else {
+            try {
 //            System.out.println("Handle request from " + input.available());
-            int message_size = input.readInt();  // request header
-            System.out.println("request message_size=" + message_size);
-            if (message_size == 0) {
-                System.out.println("end handleSequentialRequest for message_size=" + message_size);
-                return;
+                int message_size = input.readInt();  // request header
+                System.out.println("request message_size=" + message_size);
+
+                short request_api_key = input.readShort();
+                System.out.println("api_key=" + request_api_key);
+                short request_api_version = input.readShort();
+                System.out.println("api_version=" + request_api_version);
+                int correlation_id = input.readInt(); // will be in response header
+                System.out.println("correlation_id=" + correlation_id);
+                byte[] request_body = new byte[message_size - 8];
+                System.out.println("request_body len=" + request_body.length);
+                input.readFully(request_body);
+                System.out.println("request_body=" + Arrays.toString(request_body));
+
+                short error_code;
+                ByteArrayOutputStream response_body = new ByteArrayOutputStream();
+                if (request_api_version < 0 || request_api_version > 4) {
+                    error_code = 35;
+                    response_body.write(shortToByteArray(error_code));
+                } else {
+                    error_code = 0;
+                    response_body.write(shortToByteArray(error_code));
+                    response_body.write(2); // array size + 1
+                    response_body.write(request_api_key); // api_key (RequestKey.API_VERSIONS.type)
+                    response_body.write(shortToByteArray((short) 3));  // min version
+                    response_body.write(shortToByteArray((short) 4));  // max version
+                    response_body.write(0); // tagged fields
+                    response_body.write(intToByteArray(1)); //throttle time
+                    response_body.write(0); // tagged fields
+
+                }
+                System.out.println("response_body=" + Arrays.toString(response_body.toByteArray()));
+                int response_length = 4 + response_body.size();
+                System.out.println("response_size=" + response_length);
+
+                outputStream.write(intToByteArray(response_length));
+                outputStream.write(correlation_id);
+                outputStream.write(response_body.toByteArray());
+                outputStream.flush();
+
+                System.out.println("end processing a request");
+            } catch (EOFException e) {
+                System.err.println("EOF before reading all bytes" + e.getMessage());
             }
-            short request_api_key = input.readShort();
-            System.out.println("api_key=" + request_api_key);
-            short request_api_version = input.readShort();
-            System.out.println("api_version=" + request_api_version);
-            int correlation_id = input.readInt(); // will be in response header
-            System.out.println("correlation_id=" + correlation_id);
-            byte[] request_body = new byte[message_size - 8];
-            System.out.println("request_body len=" + request_body.length);
-            input.readFully(request_body);
-            System.out.println("request_body=" + Arrays.toString(request_body));
-
-            short error_code;
-            ByteArrayOutputStream response_body = new ByteArrayOutputStream();
-            if (request_api_version < 0 || request_api_version > 4) {
-                error_code = 35;
-                response_body.write(shortToByteArray(error_code));
-            } else {
-                error_code = 0;
-                response_body.write(shortToByteArray(error_code));
-                response_body.write(2); // array size + 1
-                response_body.write(request_api_key); // api_key (RequestKey.API_VERSIONS.type)
-                response_body.write(shortToByteArray((short) 3));  // min version
-                response_body.write(shortToByteArray((short) 4));  // max version
-                response_body.write(0); // tagged fields
-                response_body.write(intToByteArray(1)); //throttle time
-                response_body.write(0); // tagged fields
-
-            }
-            System.out.println("response_body=" + Arrays.toString(response_body.toByteArray()));
-            int response_length = 4 + response_body.size();
-            System.out.println("response_size=" + response_length);
-
-            outputStream.write(intToByteArray(response_length));
-            outputStream.write(correlation_id);
-            outputStream.write(response_body.toByteArray());
-            outputStream.flush();
-
-            System.out.println("end processing a request");
-        }
-        catch (EOFException e) {
-            System.err.println("EOF before reading all bytes" + e.getMessage());
         }
     }
 
